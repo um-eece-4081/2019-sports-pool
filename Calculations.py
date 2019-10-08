9a5ceba6ec8a627fe0af837556a5bc122d72d688
Shashwat Patra <spatra@memphis.edu>
2019-10-08 09:33:10 -0500

    Added some calculation functions

--- Calculations.py
+++ Calculations.py
@@ -1,2 +1,10 @@
 #The calculations methods will reside here.
 #Let's get this bread
+
+def calcWinner(team1Score, team2Score, betLine):
+    return team1Score > (team2Score + betLine)
+
+def calcUserPoints(userChoice, team1Score, team2Score, betLine):
+    if(userChoice == team1 && calcWinner(team1Score, team2Score, betline)):
+        userPoint += 1
+    return userPoint
