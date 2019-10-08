d5d634c6d3f412ccc4315476f1d728fbff242588
Shashwat Patra <spatra@memphis.edu>
2019-10-08 09:56:47 -0500

    Replaced '&&' with 'and'

--- Calculations.py
+++ Calculations.py
@@ -5,6 +5,6 @@ def calcWinner(team1Score, team2Score, betLine):
     return team1Score > (team2Score + betLine)
 
 def calcUserPoints(userChoice, team1Score, team2Score, betLine):
-    if(userChoice == team1 && calcWinner(team1Score, team2Score, betline)):
+    if(userChoice == team1 and calcWinner(team1Score, team2Score, betline)):
         userPoint += 1
     return userPoint
