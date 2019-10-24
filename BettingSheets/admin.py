59f344552cef2f8d91f99ba7535fbb6173e4331a
tylerbrycew <tbwllace@memphis.edu>
2019-10-24 17:21:30 -0500

    Fixed a bug

--- BettingSheets/admin.py
+++ BettingSheets/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from .models import MasterBettingSheet
+from .models import Game
 
 # Register your models here.
 admin.site.register(MasterBettingSheet)
+admin.site.register(Game)
 admin.site.site_header = 'Manage Betting Sheets'
