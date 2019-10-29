2719b0f4fe6887c1ac80e149ff645173d96fdb38
mdwrds14 <48966382+mdwrds14@users.noreply.github.com>
2019-10-29 09:18:08 -0500

    Update Calculations.py

--- Calculations.py
+++ Calculations.py
@@ -3,34 +3,34 @@ import User
 #Let's get this bread
 
 
-def calcWinner(team1Score, team2Score, betLine):
+def calc_winner(team1Score, team2Score, betLine):
     return team1Score > (team2Score + betLine)
 
-def calcUserPoints(user,userChoice, team1Score, team2Score, betLine):
+def calc_user_points(user,userChoice, team1Score, team2Score, betLine):
     user.userPoint = get_weekly_points()
     if(userChoice == team1 and calcWinner(team1Score, team2Score, betline) or userChoice == team2 and not calcWinnter(team1Score, team2Score, betline)):
         userPoint += 1
     user.set_weekly_points(userPoint)
     return userPoint
 
-def UserBetPoints(SeasonUserWin,UserScore,WeeklyUserWin,SeasonUser,WeeklyUser,WeeklyUserGOWWin,SeasonUserGOWWin): #Basic Point Layout
-    if SeasonUser&SeasonUserWin:
-        UserScore+1
+def user_bet_points(season_user_win,user_score,weekly_user_win,season_user,weekly_user,weekly_user_gow_win,season_user_gow_win): #Basic Point Layout
+    if season_user & season_user_win:
+        user_score+1
     else:
-        UserScore
+        user_score
 
-    if WeeklyUser&WeeklyUserWin:
-        UserScore+1
+    if weekly_user & weekly_user_win:
+        user_score+1
     else:
-        UserScore
+        user_score
 
-    if WeeklyUser & WeeklyUserGOWWin:
-        UserScore + 2
+    if weekly_user & weekly_user_gow_win:
+        user_score + 2
     else:
-        UserScore
+        user_score
 
-    if SeasonUser & SeasonUserGOWWin:
-        UserScore + 2
+    if season_user & season_user_gow_win:
+        user_score + 2
     else:
-        UserScore
-        return UserScore
+        user_score
+        return user_score
