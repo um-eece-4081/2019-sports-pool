a8c9506637b5f58ff3071648cfdd456e46dc5f8f
srpatra1 <55039259+srpatra1@users.noreply.github.com>
2019-10-31 09:24:04 -0500

    Update Calculations.py

--- Calculations.py
+++ Calculations.py
@@ -20,26 +20,28 @@ def game_of_the_week(user, actual_score):
 
 def score_all_users(user_list):
     for user in user_list:
-        
+        user_choice = get_user_choices(user)
+        calc_weekly_points(user,user_choice[0],user_choice[1],user_choice[2],betLine)
+        game_of_the_week(user, actual_score)
 
-def user_bet_points(season_user_win,user_score,weekly_user_win,season_user,weekly_user,weekly_user_gow_win,season_user_gow_win): #Basic Point Layout
-    if season_user & season_user_win:
-        user_score+1
-    else:
-        user_score
+## def user_bet_points(season_user_win,user_score,weekly_user_win,season_user,weekly_user,weekly_user_gow_win,season_user_gow_win): #Basic Point Layout
+   ## if season_user & season_user_win:
+     ##   user_score+1
+    ##else:
+      ##  user_score
 
-    if weekly_user & weekly_user_win:
-        user_score+1
-    else:
-        user_score
+    ##if weekly_user & weekly_user_win:
+      ##  user_score+1
+    ##else:
+      ##  user_score
 
-    if weekly_user & weekly_user_gow_win:
-        user_score + 2
-    else:
-        user_score
+    ##if weekly_user & weekly_user_gow_win:
+      ##  user_score + 2
+    ##else:
+      ##  user_score
 
-    if season_user & season_user_gow_win:
-        user_score + 2
-    else:
-        user_score
-        return user_score
+    ##if season_user & season_user_gow_win:
+      ##  user_score + 2
+    ##else:
+      ##  user_score
+        ##return user_score
