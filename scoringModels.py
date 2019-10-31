1a96313b71bc289142fe4dd350a656c5bbc7f059
Sam Horne <samhorne2@gmail.com>
2019-10-31 08:51:16 -0500

    change name to avoid conflict

--- scoringModels.py
+++ scoringModels.py
@@ -8,20 +8,6 @@ class User(models.Model):
     winston_points = models.PositiveIntegerField(null=True, blank=False, default=None)
     user_id = models.PositiveIntegerField(null=True, blank=False, default=None)
     currentPoints = models.PositiveIntegerField(null=True, blank=False, default=None)
-    def get_is_winston_cup(self):
-        return self.is_winston_cup
-    def get_betting_sheet(self):
-        return self.betting_sheet
-    def get_weekly_points(self):
-        return self.weekly_points
-    def get_winston_points(self):
-        return self.winston_points
-    def set_weekly_points(self, we_points):
-        self.weekly_points = w_points
-    def set_winston_points(self, wi_points):
-        self.winston_points = wi_points
-    def get_user_id(self):
-        return self.user_id
 
 for user in list:
     user.get_weekly_points
