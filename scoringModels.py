6c14a76369043203d2dcba0dd5614d8960c62978
Sam Horne <samhorne2@gmail.com>
2019-11-04 18:42:17 -0600

    Rename user as "Bettor"

--- scoringModels.py
+++ scoringModels.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class User(models.Model):
+class Bettor(models.Model):
     #def __init__(self, is_winston_cup, betting_sheet, weekly_points, winston_points, user_id):
     is_winston_cup = models.BooleanField(default=False)
     betting_sheet = models.ForeignKey('MasterBettingSheet', on_delete=models.CASCADE, null=True, verbose_name='Master Betting Sheet')
