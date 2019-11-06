830b5ea34fb95e3304eea90f78dc7725703e0787
tnelms1 <48038786+tnelms1@users.noreply.github.com>
2019-11-05 23:42:34 -0600

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
-from .managers import CustomUserManager
+from .managers import CustomUserManager 
 
 
 class Bettor(AbstractUser):
