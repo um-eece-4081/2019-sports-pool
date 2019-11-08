95a1019ea2cde6ba386a57422f5aa7c761a2f93a
srpatra1 <55039259+srpatra1@users.noreply.github.com>
2019-11-07 18:53:17 -0600

    Update models.py

--- BettingSheets/models.py
+++ BettingSheets/models.py
@@ -1,5 +1,8 @@
 from django.db import models, transaction
 from django.contrib import admin
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
 # Create your models here.
 
 
